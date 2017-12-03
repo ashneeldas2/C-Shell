a94cc3b780fe078ca97968aa11d27247575b8808
anish2454 <shenoy.anish.227@gmail.com>
2017-12-03 11:59:25 -0500

    Added Modularity

---
+++ custom_commands.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "shell.h"
+#include "parse.h"
+#include "custom_commands.h"
+
+int my_cd(char** args){
+  if(chdir(args[1])){
+    return 0;
+  }
+  else{
+    return 1;
+  }
+}
+
+int my_exit(){
+  exit(0);
+}
---
+++ custom_commands.h
@@ -0,0 +1,5 @@
+#ifndef CUSTOM_COMMANDS_H
+  #define CUSTOM_COMMANDS_H
+  int my_cd(char ** args);
+  int my_exit();
+#endif
